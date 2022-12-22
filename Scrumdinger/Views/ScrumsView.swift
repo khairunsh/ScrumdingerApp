e5ac535f61abdbc53f3ab75d7f6c1de4536e8400
Khairun Nashirin <k.nashirin1910@gmail.com>
2022-12-22 10:25:24 +0700

    Displaying data in a List

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Shii on 20/12/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+            .previewDevice("iPhone 14 Pro")
+    }
+}
---
+++ Scrumdinger/Views/Utils/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Shii on 19/12/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
